911ff37f1fb52a1b1984dc99b57747941a703259
ons-swadhin <ons.swadhin@gmail.com>
2020-06-20 20:40:13 +0530

    Added one more Sopln statement

---
+++ src/com/swadhin/hello/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.swadhin.hello;
+
+/**
+ * @author Swadhin
+ * @Created Date 20-Jun-2020
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World !");
+		System.out.println("Hello Swadhin");
+
+	}
+
+}
